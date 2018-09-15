094ae5c92c10209e2db13a461484a170c28ff49e
rupatel <patel.ru@husky.neu.edu>
2018-09-15 18:47:17 -0400

    adding index.html hello world

---
+++ src/main/java/com/example/CS5610fall2018javaserverrpatel/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.CS5610fall2018javaserverrpatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/CS5610fall2018javaserverrpatel/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.CS5610fall2018javaserverrpatel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
