2dd3394cdc0dc357d61a944d9110264ea624137f
rupatel <patel.ru@husky.neu.edu>
2018-09-15 21:31:38 -0400

    Hello service CRUD

--- src/main/java/com/neu/cs5610/fall18/course/manager/services/HelloService.java
+++ src/main/java/com/neu/cs5610/fall18/course/manager/services/HelloService.java
@@ -1,7 +1,11 @@
 package com.neu.cs5610.fall18.course.manager.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neu.cs5610.fall18.course.manager.entities.Hello;
@@ -15,4 +19,17 @@ public class HelloService {
 	public Iterable<Hello> findAllHellos() {
 		return repository.findAll();
 	}
+	
+	@PostMapping("/api/hello")
+	public Hello createHello
+	(@RequestBody Hello hello) {
+			return repository.save(hello);
+	}
+	
+	@DeleteMapping("/api/hello/{helloId}")
+	public void deleteHello
+	(@PathVariable("helloId") int id) {
+			repository.deleteById(id);
+	}
+
 }
