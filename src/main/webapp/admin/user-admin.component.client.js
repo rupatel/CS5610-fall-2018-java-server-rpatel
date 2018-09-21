5c2e5161425ff60defc56943c29cb357d451c4b6
rupatel <patel.ru@husky.neu.edu>
2018-09-20 21:10:09 -0400

    adding static user add and render feature

--- src/main/webapp/admin/user-admin.component.client.js
+++ src/main/webapp/admin/user-admin.component.client.js
@@ -6,13 +6,70 @@
     var userService = new AdminUserServiceClient();
     $(main);
 
-    function main() {  }
-    function createUser() {  }
-    function findAllUsers() {  }
-    function findUserById() {  }
+    function main() {
+        $usernameFld = $("#usernameFld");
+        $passwordFld = $("#passwordFld");
+        $removeBtn = $("#removeBtn");
+        $editBtn = $("#editBtn");
+        $createBtn = $("#createBtn");
+        $firstNameFld = $("#firstNameFld");
+        $lastNameFld = $("#lastNameFld");
+        $userRowTemplate = $(".wbdv-tbody .wbdv-template");
+        $tbody = $("tbody");
+        $roleFld = $("#roleFld");
+        $createBtn.click(createUser)
+        renderUsers(userService.findAllUsers())
+    }
+    function createUser() {
+        var u = {
+            username:$usernameFld.val(),
+            firstName:$firstNameFld.val(),
+            lastName:$lastNameFld.val(),
+            role:$roleFld.val(),
+            password:$passwordFld.val()
+        }
+        var userRow = $userRowTemplate.clone().removeClass("wbdv-hidden");
+        console.log(u);
+        userRow
+            .find(".wbdv-username")
+            .html(u.username);
+        userRow
+            .find(".wbdv-first-name")
+            .html(u.firstName);
+        userRow
+            .find(".wbdv-last-name")
+            .html(u.lastName);
+        userRow
+            .find(".wbdv-role")
+            .html(u.role);
+        $tbody.append(userRow);
+    }
+    function findAllUsers() {
+
+    }
+    function findUserById(id) {
+        return
+    }
     function deleteUser() {  }
     function selectUser() {  }
     function updateUser() {  }
     function renderUser(user) { }
-    function renderUsers(users) {  }
+    function renderUsers(users) {
+        for(i in users){
+            var userRow = $userRowTemplate.clone().removeClass("wbdv-hidden");
+            userRow
+                .find(".wbdv-username")
+                .html(users[i].username);
+            userRow
+                .find(".wbdv-first-name")
+                .html(users[i].firstName);
+            userRow
+                .find(".wbdv-last-name")
+                .html(users[i].lastName);
+            userRow
+                .find(".wbdv-role")
+                .html(users[i].role);
+            $tbody.append(userRow);
+        }
+    }
 })();
--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -7,8 +7,17 @@ function AdminUserServiceClient() {
     this.url = 'http://localhost:8080/api/user';
     var self = this;
     function createUser(user, callback) {  }
-    function findAllUsers(callback) {  }
-    function findUserById(userId, callback) {  }
+    function findAllUsers(callback) {
+        return [
+            {username:'alice',firstName:'alice',lastName:'wonderland',role:'STUDENT'},
+            {username:'malice',firstName:'malice',lastName:'cena',role:'STUDENT'},
+            {username:'jose',firstName:'jose',lastName:'annuzi',role:'FACULTY'}
+        ]
+    }
+    function findUserById(userId, callback) {
+        var user = {username:'alice',firstName:'alice',lastName:'wonderland',role:'student'};
+        callback(user);
+    }
     function updateUser(userId, user, callback) {  }
     function deleteUser(userId, callback) {  }
 }
