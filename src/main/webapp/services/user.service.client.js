84d5b5e10f397eefde38ff516aa668db4fded362
rupatel <patel.ru@husky.neu.edu>
2018-09-22 00:49:34 -0400

    service using fetch and promise

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,20 +4,72 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'http://localhost:8080/api/user';
+    this.url = 'http://localhost:8080';
     var self = this;
-    function createUser(user, callback) {  }
+
+    //will fire http post
+    function createUser(user) {
+        return fetch(
+            this.url + '/admin' + '/data' + '/findUserById.json',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => user);
+    }
+
+    //will fire http get
     function findAllUsers(callback) {
-        return [
-            {username:'alice',firstName:'alice',lastName:'wonderland',role:'STUDENT'},
-            {username:'malice',firstName:'malice',lastName:'cena',role:'STUDENT'},
-            {username:'jose',firstName:'jose',lastName:'annuzi',role:'FACULTY'}
-        ]
+        return fetch(
+            this.url + '/admin' +  '/data' + '/findAllUsers.json',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => res.json())
     }
-    function findUserById(userId, callback) {
-        var user = {username:'alice',firstName:'alice',lastName:'wonderland',role:'student'};
-        callback(user);
+
+    // will fire http get
+    function findUserById(userId) {
+        return fetch(
+            this.url + '/admin' + '/data' + '/findUserById.json',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res =>
+            res.json())
     }
-    function updateUser(userId, user, callback) {  }
-    function deleteUser(userId, callback) {  }
+
+    // will fire http post request on server
+    function updateUser(userId, user) {
+        return fetch(
+            this.url + '/admin' + '/data' + '/findUserById.json',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => user)
+    }
+
+    // will fire http delete request on server
+    function deleteUser(userId) {
+        return fetch(
+            this.url + '/admin' + '/data' + '/findUserById.json',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => res.json())
+    }
 }
