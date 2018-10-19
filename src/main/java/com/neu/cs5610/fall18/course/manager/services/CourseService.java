63b4e0161c10e86aa16bf3071bc41db78baf531d
rupatel <patel.ru@husky.neu.edu>
2018-10-19 05:39:03 -0400

    course service

--- src/main/java/com/neu/cs5610/fall18/course/manager/services/CourseService.java
+++ src/main/java/com/neu/cs5610/fall18/course/manager/services/CourseService.java
@@ -1,14 +1,77 @@
 package com.neu.cs5610.fall18.course.manager.services;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.neu.cs5610.fall18.course.manager.entities.Course;
+import com.neu.cs5610.fall18.course.manager.entities.Faculty;
+import com.neu.cs5610.fall18.course.manager.entities.Module;
+import com.neu.cs5610.fall18.course.manager.entities.Section;
 import com.neu.cs5610.fall18.course.manager.repositories.CourseRepository;
 
 @Service
+@RestController
 public class CourseService {
 	@Autowired
 	private CourseRepository courseRepo;
 	
+	@PostMapping("/api/course")
+	public Course createCourse(@RequestBody Course course, HttpSession session) {
+		course.setFaculty((Faculty)session.getAttribute("currentUser"));
+		return courseRepo.save(course);
+	}
 	
+	@GetMapping("/api/course")
+	public List<Course> findAllCourses(){
+		return (List<Course>) courseRepo.findAll();
+	}
+	
+	@GetMapping("/api/course/{courseId}")
+	public Course findCourseById(@PathVariable("courseId") Long courseId) {
+		Optional<Course> opt = courseRepo.findById(courseId);
+		if(opt.isPresent())
+			return opt.get();
+		else 
+			return null;
+	}
+	
+	@PutMapping("/api/course/{courseId}")
+	public Course updateCourse(@PathVariable("courseId") Long courseId, @RequestBody Course course) {
+		Optional<Course> opt = courseRepo.findById(courseId);
+		if(opt.isPresent())
+		{
+			Course old = opt.get();
+			Set<Module> modules = old.getModules();
+			modules.clear();
+			modules.addAll(course.getModules() != null ? course.getModules() : new HashSet());
+			Set<Section> sections = old.getSections();
+			sections.addAll(course.getSections() != null ? course.getSections() : new HashSet());			
+			course.setId(old.getId());
+			course.setModules(modules);
+			course.setSections(sections);
+			return courseRepo.save(course);
+		}
+		else 
+			return null;
+	}
+	
+	@DeleteMapping("/api/course/{courseId}")
+	public void deleteCourse(@PathVariable("courseId") Long courseId) {
+		if(courseRepo.existsById(courseId))
+			courseRepo.deleteById(courseId);
+	}
 }
